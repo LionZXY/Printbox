45c9dc48c75b5868418311e9ad0756cac17ff8aa
Nikita Kulikov <lionzxy@yandex-team.ru>
2018-07-24 19:49:57 +0300

    Fix URL

--- app/src/main/java/ru/lionzxy/printbox/utils/Constants.kt
+++ app/src/main/java/ru/lionzxy/printbox/utils/Constants.kt
@@ -5,13 +5,13 @@ import java.net.URLEncoder
 import java.util.regex.Pattern
 
 object Constants {
-    const val BASE_URL = "http://dmitry.fopf-print.ru/"
+    const val BASE_URL = "http://printbox.io/"
     const val VKFLOW_URL = "${BASE_URL}login/vk-oauth2/"
     const val REDIR_URL = "${BASE_URL}complete/vk-oauth2/"
     const val COOKIE_SESSION = "sessionid"
     const val BACKPRESS_TIMEOUT = 3000L
     const val PREFERENCE_FIRSTRUN = "firstrun"
-    const val UPLOAD_FILE_URL = "${BuildConfig.API_URL}user_documents/"
+    const val UPLOAD_FILE_URL = "${BuildConfig.API_URL}user_documents/upload_files/"
 
     val LOGIN_PATTERN = Pattern.compile("([a-zA-Z0-9]+)")
     const val PASSWORD_MIN_LENGHT = 8
